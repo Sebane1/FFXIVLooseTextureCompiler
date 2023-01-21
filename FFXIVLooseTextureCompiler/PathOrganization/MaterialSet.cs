f3283da98983100b556c969fb2133d21c4c6ab80
Sebastina <sebane1@thavea.com>
2023-01-21 00:55:29 -0600

    Adds the ability to save projects, adds much more flexibility to exports.

---
+++ FFXIVLooseTextureCompiler/PathOrganization/MaterialSet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFXIVLooseTextureCompiler.PathOrganization
+{
+    public class MaterialSet
+    {
+        string materialSetName;
+
+        string diffuse;
+        string normal;
+        string multi;
+
+        string internalDiffusePath;
+        string internalNormalPath;
+        string internalMultiPath;
+
+        public string MaterialSetName { get => materialSetName; set => materialSetName = value; }
+        public string Diffuse { get => diffuse; set => diffuse = value; }
+        public string Normal { get => normal; set => normal = value; }
+        public string Multi { get => multi; set => multi = value; }
+        public string InternalDiffusePath { get => internalDiffusePath; set => internalDiffusePath = value; }
+        public string InternalNormalPath { get => internalNormalPath; set => internalNormalPath = value; }
+        public string InternalMultiPath { get => internalMultiPath; set => internalMultiPath = value; }
+
+        public override string ToString() {
+            return materialSetName;
+        }
+    }
+}
---
+++ FFXIVLooseTextureCompiler/PathOrganization/ProjectFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFXIVLooseTextureCompiler.PathOrganization {
+    public class ProjectFile {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Website { get; set; }
+        public string Version { get; set; }
+
+        public List<MaterialSet> MaterialSets { get; set; }
+    }
+}
