c0cda81f2a688c9951c002d8e63d8e77a3686f57
Sebastina <sebane1@thavea.com>
2023-01-26 17:23:38 -0600

    Fix issue where subraces were not getting correctly detected in validation, resulting in paths not being adjusted correctly.

--- FFXIVLooseTextureCompiler/ImageProcessing/Normal.cs
+++ FFXIVLooseTextureCompiler/ImageProcessing/Normal.cs
@@ -10,7 +10,6 @@ namespace FFXIVLooseTextureCompiler.ImageProcessing {
     public class Normal {
         public static Bitmap Calculate(string file) {
             using (Bitmap image = (Bitmap)Bitmap.FromFile(file)) {
-                #region Global Variables
                 int w = image.Width - 1;
                 int h = image.Height - 1;
                 float sample_l;
@@ -20,17 +19,16 @@ namespace FFXIVLooseTextureCompiler.ImageProcessing {
                 float x_vector;
                 float y_vector;
                 Bitmap normal = new Bitmap(image.Width, image.Height);
-                #endregion
                 for (int y = 0; y < h; y++) {
                     for (int x = 0; x < w; x++) {
-                        if (x > 0) { sample_l = image.GetPixel(x - 1, y).GetBrightness(); } else { sample_l = image.GetPixel(x < w ? x : x - 1, y).GetBrightness(); }
-                        if (x < w) { sample_r = image.GetPixel(x + 1, y).GetBrightness(); } else { sample_r = image.GetPixel(x < w ? x : x - 1, y).GetBrightness(); }
-                        if (y > 1) { sample_u = image.GetPixel(x < w ? x : x - 1, y - 1).GetBrightness(); } else { sample_u = image.GetPixel(x < w ? x : x - 1, y).GetBrightness(); }
-                        if (y < h) { sample_d = image.GetPixel(x < w ? x : x - 1, y + 1).GetBrightness(); } else { sample_d = image.GetPixel(x < w ? x : x - 1, y).GetBrightness(); }
+                        if (x > 0) { sample_l = image.GetPixel(x - 1, y).GetBrightness(); } else { sample_l = image.GetPixel(x, y).GetBrightness(); }
+                        if (x < w) { sample_r = image.GetPixel(x + 1, y).GetBrightness(); } else { sample_r = image.GetPixel(x, y).GetBrightness(); }
+                        if (y > 1) { sample_u = image.GetPixel(x, y - 1).GetBrightness(); } else { sample_u = image.GetPixel(x, y).GetBrightness(); }
+                        if (y < h) { sample_d = image.GetPixel(x, y + 1).GetBrightness(); } else { sample_d = image.GetPixel(x, y).GetBrightness(); }
                         x_vector = (((sample_l - sample_r) + 1) * .5f) * 255;
                         y_vector = (((sample_u - sample_d) + 1) * .5f) * 255;
                         Color col = Color.FromArgb(255, (int)x_vector, (int)y_vector, 255);
-                        normal.SetPixel(x < w ? x : x - 1, y, col);
+                        normal.SetPixel(x, y, col);
                     }
                 }
                 return normal;
