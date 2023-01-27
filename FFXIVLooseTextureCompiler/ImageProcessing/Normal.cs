a1c50de83a6260f4b2cca8c18a9d938b3367db5a
Sebastian Lawe <sebane1@thavea.com>
2023-01-26 20:23:18 -0600

    Make normal generation MUCH more efficient.

--- FFXIVLooseTextureCompiler/ImageProcessing/Normal.cs
+++ FFXIVLooseTextureCompiler/ImageProcessing/Normal.cs
@@ -19,18 +19,40 @@ namespace FFXIVLooseTextureCompiler.ImageProcessing {
                 float x_vector;
                 float y_vector;
                 Bitmap normal = new Bitmap(image.Width, image.Height);
+                LockBitmap source = new LockBitmap(image);
+                LockBitmap destination = new LockBitmap(normal);
+                source.LockBits();
+                destination.LockBits();
                 for (int y = 0; y < h; y++) {
                     for (int x = 0; x < w; x++) {
-                        if (x > 0) { sample_l = image.GetPixel(x - 1, y).GetBrightness(); } else { sample_l = image.GetPixel(x, y).GetBrightness(); }
-                        if (x < w) { sample_r = image.GetPixel(x + 1, y).GetBrightness(); } else { sample_r = image.GetPixel(x, y).GetBrightness(); }
-                        if (y > 1) { sample_u = image.GetPixel(x, y - 1).GetBrightness(); } else { sample_u = image.GetPixel(x, y).GetBrightness(); }
-                        if (y < h) { sample_d = image.GetPixel(x, y + 1).GetBrightness(); } else { sample_d = image.GetPixel(x, y).GetBrightness(); }
+                        if (x > 0) {
+                            sample_l = source.GetPixel(x - 1, y).GetBrightness();
+                        } else {
+                            sample_l = source.GetPixel(x, y).GetBrightness();
+                        }
+                        if (x < w) {
+                            sample_r = source.GetPixel(x + 1, y).GetBrightness();
+                        } else {
+                            sample_r = source.GetPixel(x, y).GetBrightness();
+                        }
+                        if (y > 1) {
+                            sample_u = source.GetPixel(x, y - 1).GetBrightness();
+                        } else {
+                            sample_u = source.GetPixel(x, y).GetBrightness();
+                        }
+                        if (y < h) {
+                            sample_d = source.GetPixel(x, y + 1).GetBrightness();
+                        } else {
+                            sample_d = source.GetPixel(x, y).GetBrightness();
+                        }
                         x_vector = (((sample_l - sample_r) + 1) * .5f) * 255;
                         y_vector = (((sample_u - sample_d) + 1) * .5f) * 255;
                         Color col = Color.FromArgb(255, (int)x_vector, (int)y_vector, 255);
-                        normal.SetPixel(x, y, col);
+                        destination.SetPixel(x, y, col);
                     }
                 }
+                destination.UnlockBits();
+                source.UnlockBits();
                 return normal;
             }
         }
@@ -46,21 +68,26 @@ namespace FFXIVLooseTextureCompiler.ImageProcessing {
             float sample_d;
             float x_vector;
             float y_vector;
-            Bitmap finalNormal = new Bitmap(image.Width, image.Height);
             Bitmap normal = new Bitmap(image.Width, image.Height);
+            LockBitmap source = new LockBitmap(image);
+            LockBitmap destination = new LockBitmap(normal);
+            source.LockBits();
+            destination.LockBits();
             #endregion
             for (int y = 0; y < h + 1; y++) {
                 for (int x = 0; x < w + 1; x++) {
-                    if (x > 0) { sample_l = image.GetPixel(x - 1, y).GetBrightness(); } else { sample_l = image.GetPixel(x, y).GetBrightness(); }
-                    if (x < w) { sample_r = image.GetPixel(x + 1, y).GetBrightness(); } else { sample_r = image.GetPixel(x, y).GetBrightness(); }
-                    if (y > 1) { sample_u = image.GetPixel(x, y - 1).GetBrightness(); } else { sample_u = image.GetPixel(x, y).GetBrightness(); }
-                    if (y < h) { sample_d = image.GetPixel(x, y + 1).GetBrightness(); } else { sample_d = image.GetPixel(x, y).GetBrightness(); }
+                    if (x > 0) { sample_l = source.GetPixel(x - 1, y).GetBrightness(); } else { sample_l = source.GetPixel(x, y).GetBrightness(); }
+                    if (x < w) { sample_r = source.GetPixel(x + 1, y).GetBrightness(); } else { sample_r = source.GetPixel(x, y).GetBrightness(); }
+                    if (y > 1) { sample_u = source.GetPixel(x, y - 1).GetBrightness(); } else { sample_u = source.GetPixel(x, y).GetBrightness(); }
+                    if (y < h) { sample_d = source.GetPixel(x, y + 1).GetBrightness(); } else { sample_d = source.GetPixel(x, y).GetBrightness(); }
                     x_vector = (((sample_l - sample_r) + 1) * .5f) * 255;
                     y_vector = (((sample_u - sample_d) + 1) * .5f) * 255;
                     Color col = Color.FromArgb(255, (int)x_vector, (int)y_vector, 255);
-                    normal.SetPixel(x, y, col);
+                    destination.SetPixel(x, y, col);
                 }
             }
+            destination.UnlockBits();
+            source.UnlockBits();
             normal.RotateFlip(RotateFlipType.RotateNoneFlipX);
             Bitmap normal2 = new Bitmap(normal);
             KVImage.ImageBlender imageBlender = new KVImage.ImageBlender();
