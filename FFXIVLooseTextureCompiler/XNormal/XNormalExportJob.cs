0f902917f8195ad7bd51878ce49a11b122a26773
Sebastian Lawe <sebane1@thavea.com>
2023-03-12 16:03:33 -0500

    Update to v1.0.5.5

--- FFXIVLooseTextureCompiler/XNormal/XNormalExportJob.cs
+++ FFXIVLooseTextureCompiler/XNormal/XNormalExportJob.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FFXIVLooseTextureCompiler.Export {
+namespace FFXIVLooseTextureCompiler {
     public class XNormalExportJob {
         private string internalPath;
         private string inputTexturePath;
