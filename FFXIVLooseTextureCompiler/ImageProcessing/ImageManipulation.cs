83d340569c98e50215c249979d6b5f16f4bc0322
Sebastian Lawe <sebane1@thavea.com>
2023-03-17 01:12:27 -0500

    Make it so that XNormal generation for overlays is split into two different maps.

--- FFXIVLooseTextureCompiler/ImageProcessing/ImageManipulation.cs
+++ FFXIVLooseTextureCompiler/ImageProcessing/ImageManipulation.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SixLabors.ImageSharp.ColorSpaces;
+using System;
 using System.Collections.Generic;
 using System.Drawing.Imaging;
 using System.Linq;
@@ -23,7 +24,66 @@ namespace FFXIVLooseTextureCompiler.ImageProcessing {
             source.UnlockBits();
             return image;
         }
- 
+
+        public static Bitmap Resize(Bitmap file, int width, int height) {
+            Bitmap image = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            Graphics g = Graphics.FromImage(image);
+            g.Clear(Color.Transparent);
+            g.DrawImage(file, 0, 0, width, height);
+            return image;
+        }
+
+        public static Bitmap ExtractTransparency(Bitmap file) {
+            Bitmap image = new Bitmap(file);
+            LockBitmap source = new LockBitmap(image);
+            source.LockBits();
+            for (int y = 0; y < image.Height; y++) {
+                for (int x = 0; x < image.Width; x++) {
+                    Color sourcePixel = source.GetPixel(x, y);
+                    Color col = Color.FromArgb(255, sourcePixel.A, sourcePixel.A, sourcePixel.A);
+                    source.SetPixel(x, y, col);
+                }
+            };
+            source.UnlockBits();
+            return image;
+        }
+        public static Bitmap ExtractRGB(Bitmap file) {
+            Bitmap image = new Bitmap(file);
+            LockBitmap source = new LockBitmap(image);
+            source.LockBits();
+            for (int y = 0; y < image.Height; y++) {
+                for (int x = 0; x < image.Width; x++) {
+                    Color sourcePixel = source.GetPixel(x, y);
+                    Color col = Color.FromArgb(255, sourcePixel.R, sourcePixel.G, sourcePixel.B);
+                    source.SetPixel(x, y, col);
+                }
+            };
+            source.UnlockBits();
+            return image;
+        }
+
+        public static Bitmap MergeAlphaToRRGB(Bitmap alpha, Bitmap rgb) {
+            Bitmap image = new Bitmap(rgb.Width, rgb.Height, PixelFormat.Format32bppArgb);
+            LockBitmap destination = new LockBitmap(image);
+            LockBitmap alphaBits = new LockBitmap(alpha);
+            LockBitmap rgbBits = new LockBitmap(rgb);
+            alphaBits.LockBits();
+            rgbBits.LockBits();
+            destination.LockBits();
+            for (int y = 0; y < image.Height; y++) {
+                for (int x = 0; x < image.Width; x++) {
+                    Color alphaPixel = alphaBits.GetPixel(x, y);
+                    Color rgbPixel = rgbBits.GetPixel(x, y);
+                    Color col = Color.FromArgb(alphaPixel.R, rgbPixel.R, rgbPixel.G, rgbPixel.B);
+                    destination.SetPixel(x, y, col);
+                }
+            };
+            alphaBits.UnlockBits();
+            rgbBits.UnlockBits();
+            destination.UnlockBits();
+            return image;
+        }
+
         public static Bitmap MergeNormals(string inputFile, Bitmap diffuse, Bitmap canvasImage, Bitmap normalMask, string diffuseNormal) {
             Graphics g = Graphics.FromImage(canvasImage);
             g.Clear(Color.White);
