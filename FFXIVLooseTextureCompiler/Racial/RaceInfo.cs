3be3afb6e9a4ab35f9ea3645df145e4e13547820
Sebastina <sebane1@thavea.com>
2023-05-04 16:33:28 -0500

    Fix incorrect subraces spellings for Roegadyns

--- FFXIVLooseTextureCompiler/Racial/RaceInfo.cs
+++ FFXIVLooseTextureCompiler/Racial/RaceInfo.cs
@@ -6,7 +6,7 @@ using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 namespace FFXIVLooseTextureCompiler.Racial {
     public static class RaceInfo {
         private static List<string> subRaces = new List<string>() { "Midlander" , "Highlander","Wildwood","Duskwight","Seeker",
-            "Keeper", "Sea Wolves", "Hellsgaurd", "Plainsfolk", "Dunesfolk", "Raen", "Xaela", "Helions", "The Lost", "Rava", "Veena" };
+            "Keeper", "Sea Wolf", "Hellsguard", "Plainsfolk", "Dunesfolk", "Raen", "Xaela", "Helions", "The Lost", "Rava", "Veena" };
         private static List<string> races = new List<string>() { "Midlander","Highlander","Elezen","Miqo'te","Roegadyn",
             "Lalafell","Raen","Xaela","Hrothgar","Viera",};
 
