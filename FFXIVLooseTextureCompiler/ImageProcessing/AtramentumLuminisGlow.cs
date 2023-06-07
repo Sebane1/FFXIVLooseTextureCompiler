e9006baa30635c33b35c772869c0c87320672d7d
Sebastina <sebane1@thavea.com>
2023-06-07 12:24:39 -0500

    Improve the way glow maps interact with Atramentum Luminis

--- FFXIVLooseTextureCompiler/ImageProcessing/AtramentumLuminisGlow.cs
+++ FFXIVLooseTextureCompiler/ImageProcessing/AtramentumLuminisGlow.cs
@@ -1,9 +1,13 @@
-﻿namespace FFXIVLooseTextureCompiler.ImageProcessing {
+﻿using Lumina.Data.Parsing.Layer;
+using System.Runtime.CompilerServices;
+
+namespace FFXIVLooseTextureCompiler.ImageProcessing {
     public class AtramentumLuminisGlow {
         public static Bitmap CalculateDiffuse(Bitmap file, Bitmap glow) {
             Bitmap image = new Bitmap(glow, file.Width, file.Height);
             Bitmap diffuse = new Bitmap(file);
             Bitmap mergedImage = new Bitmap(diffuse);
+            //Bitmap debugImage = new Bitmap(file.Width, file.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
 
             Bitmap glowMultiply = new Bitmap(mergedImage);
             Graphics g = Graphics.FromImage(glowMultiply);
@@ -14,20 +18,30 @@
             LockBitmap source = new LockBitmap(image);
             LockBitmap destination = new LockBitmap(diffuse);
             LockBitmap mergedImagePixels = new LockBitmap(mergedImage);
+            //LockBitmap debug = new LockBitmap(debugImage);
+
             source.LockBits();
             destination.LockBits();
+            //debug.LockBits();
             mergedImagePixels.LockBits();
             for (int y = 0; y < image.Height; y++) {
                 for (int x = 0; x < image.Width; x++) {
                     Color sourcePixel = source.GetPixel(x, y);
                     Color mergedPixel = mergedImagePixels.GetPixel(x, y);
-                    if (sourcePixel.A > 20) {
-                        Color col = Color.FromArgb(255 - sourcePixel.A, sourcePixel.R, sourcePixel.G, sourcePixel.B);
-                        destination.SetPixel(x, y, col);
-                    } else if (sourcePixel.A > 10) {
-                        Color col = Color.FromArgb(255 - sourcePixel.A, mergedPixel.R, mergedPixel.G, mergedPixel.B);
-                        destination.SetPixel(x, y, col);
-                    } else if (sourcePixel.A > 0) {
+                    Color comparisonColour = FlattenColours(sourcePixel);
+                    if (!(comparisonColour.R == 0 && comparisonColour.G == 0 && comparisonColour.B == 0)) {
+                        if (sourcePixel.A > 20) {
+                            Color col = Color.FromArgb(255 - sourcePixel.A, sourcePixel.R, sourcePixel.G, sourcePixel.B);
+                            destination.SetPixel(x, y, col);
+                        } else if (sourcePixel.A > 10) {
+                            Color col = Color.FromArgb(255 - sourcePixel.A, mergedPixel.R, mergedPixel.G, mergedPixel.B);
+                            destination.SetPixel(x, y, col);
+                        } else if (sourcePixel.A > 0) {
+                            Color col = Color.FromArgb(mergedPixel.A, mergedPixel.R, mergedPixel.G, mergedPixel.B);
+                            destination.SetPixel(x, y, col);
+                        }
+                        //debug.SetPixel(x, y, sourcePixel);
+                    } else {
                         Color col = Color.FromArgb(mergedPixel.A, mergedPixel.R, mergedPixel.G, mergedPixel.B);
                         destination.SetPixel(x, y, col);
                     }
@@ -36,9 +50,16 @@
             destination.UnlockBits();
             source.UnlockBits();
             mergedImagePixels.UnlockBits();
+            //debug.UnlockBits();
+            //debugImage.Save(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "atramentumTest.png"));
             return diffuse;
         }
-
+        public static Color FlattenColours(Color colour, int minBrightness = 90) {
+            return Color.FromArgb(colour.A,
+                colour.R > minBrightness ? colour.R : 0,
+                colour.G > minBrightness ? colour.G : 0,
+                colour.B > minBrightness ? colour.B : 0);
+        }
         public static Bitmap CalculateEyeMulti(Bitmap file, Bitmap glow) {
             Bitmap image = new Bitmap(glow, file.Width, file.Height);
             Bitmap multi = new Bitmap(file);
@@ -71,9 +92,13 @@
             for (int y = 0; y < image.Height; y++) {
                 for (int x = 0; x < image.Width; x++) {
                     Color sourcePixel = source.GetPixel(x, y);
-                    if (sourcePixel.A > 0) {
-                        Color col = Color.FromArgb(sourcePixel.A, sourcePixel.R, sourcePixel.G, 255 - sourcePixel.A);
-                        destination.SetPixel(x, y, col);
+                    Color destinationPixel = destination.GetPixel(x, y);
+                    Color comparisonColour = FlattenColours(sourcePixel, 90);
+                    if (!(comparisonColour.R == 0 && comparisonColour.G == 0 && comparisonColour.B == 0)) {
+                        if (sourcePixel.A > 20) {
+                            Color col = Color.FromArgb(destinationPixel.A, destinationPixel.R, destinationPixel.G, 255 - sourcePixel.A);
+                            destination.SetPixel(x, y, col);
+                        }
                     }
                 }
             }
