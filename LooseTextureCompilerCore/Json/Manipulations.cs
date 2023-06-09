b71c81088aae6a580d8a6c7abe30d7a8c43a6e70
Sebastina <sebane1@thavea.com>
2023-06-09 12:29:30 -0500

    Remove LTC Core from project to be added to new repo/

--- LooseTextureCompilerCore/Json/Manipulations.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace FFXIVVoicePackCreator.Json {
-    public class Manipulations {
-        string name;
-        public string Name { get => name; set => name = value; }
-    }
-}
--- LooseTextureCompilerCore/Penumbra/ReloadData.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace FFXIVLooseTextureCompiler.Penumbra {
-    public class ReloadData {
-        public string Path { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
-    }
-}
